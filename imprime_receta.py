dace7cc36b12a18c0b09cfbc15f1455b95edf68c
s.calvo2022 <s.calvo2022@alumnos.urjc.es>
2023-04-26 12:15:19 +0200

    Añadir archivos: imprime_receta.py y recetas.md

---
+++ imprime_receta.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+
+import sys
+
+fichero=open('recetas.md')
+print(fichero.read())
+
+
+sys.exit()
---
+++ recetas.md
@@ -0,0 +1,2 @@
+Archivo practica 2 ej 2
+Segundo cambio en el archivo para conseguir incopatibilidad
